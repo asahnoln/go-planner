7ea36283f47b74580f37fd3cd2037b05d962e45c
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-09-02 00:12:12 +0600

    Seems like you don't need those structs now
    - [CHANGED] Structs unexported

--- event.go
+++ event.go
@@ -4,14 +4,14 @@ import (
 	"time"
 )
 
-// Event is an event with a start time and duration
-type Event struct {
+// event is an event with a start time and duration
+type event struct {
 	duration time.Duration
 	start    time.Time
 }
 
 // TimeRange returns a string with start and finish time of the event, like "12:00-12:05"
-func (e *Event) TimeRange() string {
+func (e *event) TimeRange() string {
 	beginRange := e.start.Format(Layout)
 	endRange := e.start.Add(e.duration).Format(Layout)
 
--- project.go
+++ project.go
@@ -6,30 +6,30 @@ import "time"
 // Layout is a basic layout for time formatting
 const Layout = "15:04"
 
-// Project presents current timetable holding events with their durations.
-type Project struct {
-	events []*Event
+// project presents current timetable holding events with their durations.
+type project struct {
+	events []*event
 	start  time.Time
 }
 
 // NewProject creates a new project with default start time 12:00
-func NewProject() *Project {
-	return &Project{
+func NewProject() *project {
+	return &project{
 		start: time.Date(2000, 1, 1, 12, 0, 0, 0, time.UTC),
 	}
 }
 
 // AddEvent adds an event to the project with given duration.
 // Use time.Duration approach to add durations, like 5 * time.Minute
-func (p *Project) AddEvent(d time.Duration) *Event {
-	e := &Event{d, p.finishTime()}
+func (p *project) AddEvent(d time.Duration) *event {
+	e := &event{d, p.finishTime()}
 	p.events = append(p.events, e)
 
 	return e
 }
 
 // StartTime changes project's start time and shifts events start times accordingly
-func (p *Project) StartTime(t time.Time) {
+func (p *project) StartTime(t time.Time) {
 	p.start = t
 	next := t
 
@@ -40,7 +40,7 @@ func (p *Project) StartTime(t time.Time) {
 }
 
 // finishTime is a convinience function to get finishing time of the project
-func (p *Project) finishTime() time.Time {
+func (p *project) finishTime() time.Time {
 	if l := len(p.events); l > 0 {
 		lastEvent := p.events[l-1]
 		return lastEvent.start.Add(lastEvent.duration)
