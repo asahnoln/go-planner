60ca553be31bc3b3b16d6bb6c92da1f45310fa36
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2022-06-04 15:23:10 +0600

    Refactored view switching
    - [CHANGED] Moved view switching to different method

--- pkg/tui/tui.go
+++ pkg/tui/tui.go
@@ -1,12 +1,43 @@
 package tui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var inputs = []struct {
+	placeholder string
+}{
+	{placeholder: "Event Description"},
+	{placeholder: "Event Duration"},
+}
+
+type View int
+
+type ViewMsg View
+
+const (
+	MainView View = iota
+	AddView
+)
 
 type Model struct {
+	Inputs  []textinput.Model
+	curView View
+	added   bool
 }
 
 func New() Model {
-	return Model{}
+	m := Model{}
+	for _, i := range inputs {
+		t := textinput.New()
+		t.Placeholder = i.placeholder
+		m.Inputs = append(m.Inputs, t)
+	}
+
+	return m
 }
 
 func (m Model) Init() tea.Cmd {
@@ -17,13 +48,43 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "q", "ctrl+c", "esc":
 			return m, tea.Quit
+		case "a":
+			return m.switchView(AddView)
+		case "enter":
+			if m.curView == AddView {
+				m.added = true
+				return m.switchView(MainView)
+			}
 		}
+	case ViewMsg:
+		return m.switchView(View(msg))
 	}
 	return m, nil
 }
 
 func (m Model) View() string {
-	return "Planner"
+	var b strings.Builder
+	b.WriteString("Planner\n")
+
+	if m.curView == AddView {
+		for _, i := range m.Inputs {
+			b.WriteString(i.View() + "\n")
+		}
+		return b.String()
+	}
+
+	if m.added {
+		b.WriteString("Intro 5 12:00-12:05")
+	}
+	return b.String()
+}
+
+func (m Model) switchView(v View) (tea.Model, tea.Cmd) {
+	m.curView = v
+	if v == AddView {
+		m.Inputs[0].Focus()
+	}
+	return m, nil
 }
--- pkg/tui/tui_test.go
+++ pkg/tui/tui_test.go
@@ -9,57 +9,13 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInit(t *testing.T) {
 	m := tui.New()
 	assert.Contains(t, m.View(), "Planner")
 }
-
-// TODO: When pressed a - it's sent to the input
-// func TestSwitchToAddEvent(t *testing.T) {
-// 	var m tea.Model = tui.NewModel()
-// 	m, _ = m.Update(tea.KeyMsg(tea.Key{
-// 		Type:  tea.KeyRunes,
-// 		Runes: []rune{'a'},
-// 	}))
-// 	n := m.(tui.Model)
-// 	v := m.View()
-// 	require.Contains(t, v, "> ", "want text input")
-// 	assert.Contains(t, v, "Description", "want text input placeholder")
-// 	assert.Contains(t, v, "Duration", "want time input placeholder")
-// 	assert.True(t, n.EventNameInput.Focused(), "want text input focused")
-
-// }
-
-// func TestAddEvent(t *testing.T) {
-// 	var m tea.Model = tui.NewModel()
-// 	n := m.(tui.Model)
-// 	n.SwitchView(tui.AddView)
-// 	n.EventNameInput.SetValue("Intro")
-// 	n.EventDurationInput.SetValue("5")
-
-// 	m, _ = n.Update(tea.KeyMsg(tea.Key{
-// 		Type: tea.KeyEnter,
-// 	}))
-// 	v := m.View()
-// 	assert.Contains(t, v, "Planner", "want return to main page")
-// 	assert.Contains(t, v, "Intro", "want event name on the page")
-// 	assert.Contains(t, v, "5", "want event duration on the page")
-// 	assert.Contains(t, v, "12:00-12:05", "want event timings on the page")
-// }
-
-// func TestSelectInputs(t *testing.T) {
-// 	var m tea.Model = tui.NewModel()
-// 	n := m.(tui.Model)
-// 	n.SwitchView(tui.AddView)
-
-// 	m, _ = n.Update(tea.KeyMsg(tea.Key{
-// 		Type: tea.KeyTab,
-// 	}))
-// 	assert.False(t, n.EventNameInput.Focused(), "want name input blured")
-// 	// assert.True(t, n.EventDurationInput.Focused(), "want duration input focused")
-// }
 
 func TestQuit(t *testing.T) {
 	tests := []struct {
@@ -69,6 +25,8 @@ func TestQuit(t *testing.T) {
 	}{
 		{"q", tea.KeyRunes, []rune{'q'}},
 		{"ctrl+c", tea.KeyCtrlC, nil},
+		// TODO: Esc might be not good for subviews
+		{"esc", tea.KeyEsc, nil},
 	}
 
 	for _, tt := range tests {
@@ -85,3 +43,58 @@ func TestQuit(t *testing.T) {
 		})
 	}
 }
+
+// TODO: When pressed a - it's sent to the input
+func TestSwitchToAddEvent(t *testing.T) {
+	var m tea.Model = tui.New()
+	m, _ = m.Update(tea.KeyMsg(tea.Key{
+		Type:  tea.KeyRunes,
+		Runes: []rune{'a'},
+	}))
+	n := m.(tui.Model)
+	v := m.View()
+	require.Contains(t, v, "> ", "want text input")
+	assert.Contains(t, v, "Description", "want text input placeholder")
+	assert.Contains(t, v, "Duration", "want time input placeholder")
+	assert.True(t, n.Inputs[0].Focused(), "want text input focused")
+}
+
+func TestAddEvent(t *testing.T) {
+	var m tea.Model = tui.New()
+	n := m.(tui.Model)
+	n.Inputs[0].SetValue("Intro")
+	n.Inputs[1].SetValue("5")
+
+	t.Run("enter should not work on main page", func(t *testing.T) {
+		m, _ = n.Update(tui.ViewMsg(tui.MainView))
+		m, _ = m.Update(tea.KeyMsg(tea.Key{
+			Type: tea.KeyEnter,
+		}))
+		v := m.View()
+		assert.NotContains(t, v, "Intro", "want event name on the page")
+	})
+
+	t.Run("usual add", func(t *testing.T) {
+		m, _ = n.Update(tui.ViewMsg(tui.AddView))
+		m, _ = m.Update(tea.KeyMsg(tea.Key{
+			Type: tea.KeyEnter,
+		}))
+		v := m.View()
+		assert.Contains(t, v, "Planner", "want return to main page")
+		assert.Contains(t, v, "Intro", "want event name on the page")
+		assert.Contains(t, v, "5", "want event duration on the page")
+		assert.Contains(t, v, "12:00-12:05", "want event timings on the page")
+	})
+}
+
+// func TestSelectInputs(t *testing.T) {
+// 	var m tea.Model = tui.NewModel()
+// 	n := m.(tui.Model)
+// 	n.SwitchView(tui.AddView)
+
+// 	m, _ = n.Update(tea.KeyMsg(tea.Key{
+// 		Type: tea.KeyTab,
+// 	}))
+// 	assert.False(t, n.EventNameInput.Focused(), "want name input blured")
+// 	// assert.True(t, n.EventDurationInput.Focused(), "want duration input focused")
+// }
