a3017b6f18f34ff01cdadb50b29b98ea83a1f311
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2022-06-04 15:25:22 +0600

    Fix method name in main
    - [CHANGED][FIX] New method to create model instead of old one in cmd

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	if err := tea.NewProgram(tui.NewModel()).Start(); err != nil {
+	if err := tea.NewProgram(tui.New()).Start(); err != nil {
 		log.Fatalf("could not start the program: %q\n", err)
 	}
 }
